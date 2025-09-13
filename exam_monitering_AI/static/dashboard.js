320a5da38f0314d6ec9d2c17ed1e53cda0e865e2
siddhu3543 <sbarla2@gitam.in>
2025-09-13 17:16:43 +0530

    Delete exam_monitering_AI directory

--- exam_monitering_AI/static/dashboard.js
+++
@@ -1,9 +0,0 @@
-// static/dashboard.js
-const socket = io.connect("http://localhost:5000");
-const logBox = document.querySelector("#alerts");
-
-socket.on("alert", (msg) => {
-    let p = document.createElement("p");
-    p.innerText = msg;
-    logBox.appendChild(p);
-});
